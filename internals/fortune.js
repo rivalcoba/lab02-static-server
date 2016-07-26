1d62aeef20341d00aed6bb833854aee4d7fc00e8
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2016-07-26 09:02:12 -0500

    V1.0.1 Fortune Cookie con base de datos remota

--- internals/fortune.js
+++ internals/fortune.js
@@ -7,12 +7,15 @@ var mongoClient = mongodb.MongoClient;
 module.exports = {
     "getFortune" : function(cb){
         // Conectando el cliente a la base de datos fortune
-        mongoClient.connect("mongodb://127.0.0.1:27017/fortune",
+        //var connectionString = "mongodb://127.0.0.1:27017/fortune";
+        var connectionString = 
+        "mongodb://verison:itgampwm2016@ds064718.mlab.com:64718/fortune";  
+        mongoClient.connect(connectionString,
         function(err, db){
             if(err){
                 console.log("> ERROR al conectarse a" +
-                " la base de datos:"+
-                " mongodb://127.0.0.1:27017/fortune");
+                " la base de datos: "+
+                connectionString);
                 var fortunePaper = {
                     "message":
                     "La honestidad es un regalo caro, no lo esperes de gente barata"
